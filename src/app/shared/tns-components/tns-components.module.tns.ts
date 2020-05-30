36e353df874ea3bec65f78f720574280e1304e37
lavrinec <ml9987@student.uni-lj.si>
2020-05-30 11:34:53 +0200

    missing files

---
+++ src/app/shared/tns-components/tns-components.module.tns.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    NativeScriptModule,
+  ],
+  providers: [
+  ],
+})
+export class TnsComponentsModuleTns { }
