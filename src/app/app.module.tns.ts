5cfd6ed47a95782ff027ba8ea668ff6f2117c7f5
NinoP <nino.petrovic@manto.net>
2020-06-08 13:32:45 +0200

    AppModuleTns interceptors added

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -8,8 +8,12 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ComponentsModuleTns } from '@shared/components/components.module.tns';
 import { CoreModuleTns } from '@core/core.module.tns';
 import { PipesModuleTns } from '@shared/pipes/pipes.module.tns';
-import {LoginModuleTns} from "@src/app/login/login.module";
+import {LoginModuleTns} from '@src/app/login/login.module';
 import { ContactsModuleTns } from './contacts/contacts.module.tns';
+import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {BaseUrlInterceptor} from '@core/interceptors/base-url.interceptor';
+import {HeaderInterceptor} from '@core/interceptors/header.interceptor';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
@@ -30,12 +34,15 @@ import { ContactsModuleTns } from './contacts/contacts.module.tns';
     ComponentsModuleTns,
     PipesModuleTns,
     TranslateModule.forRoot(),
+    NativeScriptHttpClientModule,
     LoginModuleTns,
-      // testing
+    // testing
     ContactsModuleTns
   ],
   providers: [
-    TranslateService
+    TranslateService,
+    { provide: HTTP_INTERCEPTORS, useClass: BaseUrlInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: HeaderInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA]
