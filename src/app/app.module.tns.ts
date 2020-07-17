2c9c197ab7f1392bf2c4aa758bf78c13623c04e5
Matic Bezjak <matic.bezjak@outlook.com>
2020-07-17 19:58:02 +0200

    v10 rc implemented

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -1,5 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { NativeScriptModule } from '@nativescript/angular';
 
 import { AppRoutingModule } from '@src/app/app-routing.module.tns';
 import { AppComponent } from '@src/app/app.component';
