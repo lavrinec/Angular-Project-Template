8a689d519f6cc433775632a281e850ae4693b473
Matic Bezjak <matic.bezjak@outlook.com>
2020-09-05 21:31:04 +0200

    fix forme da ne crashne več ko je kaj null

--- src/app/components/contatcs/contacts.detail/contacts.detail.component.ts
+++ src/app/components/contatcs/contacts.detail/contacts.detail.component.ts
@@ -15,8 +15,8 @@ export class ContactsDetailComponent implements OnInit {
 
   contactDTO: ContactDTO;
   contact: Contact = new Contact();
-  predpona = ['', 'družina', 'g.'];
-  pripona = ['', 'dipl.', 'dipl. inž.'];
+  predpona = ['', 'družina', 'g.', 'ga.', 'gdč.', 'mag.', 'dr'];
+  pripona = ['',  'dipl.', 'dipl. inž.', 'univ. dipl. inž.', 'dr.', 'dr. med.', 'inž., ml.'];
 
   constructor(
     private helper: HelperService,
@@ -35,17 +35,22 @@ export class ContactsDetailComponent implements OnInit {
           this.contactDTO.id = contact.id;
           this.contactDTO.firstName = contact.firstName;
           this.contactDTO.lastName = contact.lastName;
-          this.contactDTO.prefix = contact.prefix;
           if (contact.suffix) {
             this.contactDTO.suffix = contact.suffix;
           } else {
             this.contactDTO.suffix = '';
           }
-
+          if (contact.prefix) {
+            this.contactDTO.prefix = contact.prefix;
+          } else {
+            this.contactDTO.prefix = '';
+          }
           this.contactDTO.isActive = contact.isActive;
-          this.contactDTO.birthday = contact.birthday;
-
-
+          if (contact.birthday) {
+            this.contactDTO.birthday = contact.birthday;
+          } else {
+            this.contactDTO.birthday = '';
+          }
            console.log('aaa get contact', this.contactDTO);
         });
       }
